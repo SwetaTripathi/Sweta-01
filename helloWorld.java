a60122212992cf229928a62adf5cf807e5ac9b39
Sweta Tripathi <swetatripathi97@gmail.com>
2020-07-24 12:50:35 +0530

    ready for conflict

--- helloWorld.java
+++ helloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld
      public static void main(String[] args)
             {
                 System.out.println("Hello to Terminal Command");
-                  System.out.println("Hello again");
+                  System.out.println("Hello never 2456 again");
              }
 
 }

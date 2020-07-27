526cb46581e4075e9bd6dafc048544d0c3cb02dc
Sweta Tripathi <swetatripathi97@gmail.com>
2020-07-27 11:31:04 +0530

    Master branch updation

--- helloWorld.java
+++ helloWorld.java
@@ -3,8 +3,7 @@ public class HelloWorld
 
      public static void main(String[] args)
             {
-                System.out.println("Hello to Terminal Command");
-                  System.out.println("Hello but whyyyy");
+                System.out.println("This is my master branch");
 
              }
 

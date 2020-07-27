8487cfeac74d7fa1526d1c67f0a5332a3689de89
Sweta Tripathi <swetatripathi97@gmail.com>
2020-07-27 12:34:48 +0530

    git ignore demo

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
 
      public static void main(String[] args)
             {
-                System.out.println("This is my master branch");
+                System.out0.println("This is my master branch with ignore demo");
                 System.out.println("This is my new feature implemented in develop branch")
                 System.out.println("in my feature branch");
 
